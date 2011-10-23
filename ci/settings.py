01975e942fc43a0c442c23de53f919bf6cdb971e
Bruno Renié <buburno@gmail.com>
2011-10-23 18:48:00 +0200

    Debug toolbar settings

--- ci/settings.py
+++ ci/settings.py
@@ -15,3 +15,37 @@ CELERYD_CONCURRENCY = 1
 DIRECTORIES = (
     ('', 'django-admin.py test projects --settings=ci.test_settings'),
 )
+
+# Enable django-debug-toolbar if installed
+try:
+    import debug_toolbar
+
+    INTERNAL_IPS = ('127.0.0.1',)
+
+    INSTALLED_APPS = INSTALLED_APPS + (
+        'debug_toolbar',
+    )
+
+    MIDDLEWARE_CLASSES = MIDDLEWARE_CLASSES + (
+        'debug_toolbar.middleware.DebugToolbarMiddleware',
+    )
+
+    DEBUG_TOOLBAR_CONFIG = {
+        'INTERCEPT_REDIRECTS': False,
+        'HIDE_DJANGO_SQL': False,
+    }
+
+    DEBUG_TOOLBAR_PANELS = (
+        'debug_toolbar.panels.version.VersionDebugPanel',
+        'debug_toolbar.panels.timer.TimerDebugPanel',
+        'debug_toolbar.panels.settings_vars.SettingsVarsDebugPanel',
+        'debug_toolbar.panels.headers.HeaderDebugPanel',
+        'debug_toolbar.panels.request_vars.RequestVarsDebugPanel',
+        'debug_toolbar.panels.template.TemplateDebugPanel',
+        'debug_toolbar.panels.sql.SQLDebugPanel',
+        'debug_toolbar.panels.signals.SignalDebugPanel',
+        'debug_toolbar.panels.logger.LoggingPanel',
+        'debug_toolbar.panels.cache.CacheDebugPanel',
+    )
+except ImportError:
+    pass
